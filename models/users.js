8af1f94cc31299048752f233d34a333d12451bdc
pranavrajvanshi <64026650+pranavrajvanshi@users.noreply.github.com>
2021-07-01 18:22:18 +0530

    Add files via upload

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  role: { type: String, default: "NA" },
+  accessToken: { type: String, required: true,select: false },
+  outlookId: { type: String, required: true },
+  contact: { type: Number, length: 10 },
+});
+
+module.exports = mongoose.model("User", UserSchema);
